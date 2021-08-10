2b94733fcec4e7c50ca6ccb0d620877c34d02ffb
VaishnaviiSanju <vaishoff@gmail.com>
2021-08-10 23:19:54 +0530

    JavaPjt

---
+++ src/org/xyz/C.java
@@ -0,0 +1,16 @@
+package org.xyz;
+
+public class C {
+private void add() {
+	System.out.println("add");
+
+}
+private void sub() {
+	System.out.println("sub");
+
+}
+public static void main(String[] args) {
+	C c = new C();
+	c.add();
+}
+}
